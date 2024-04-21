6984a48d276983c1f683347b8ec1ac3b6c9719ea
Abhishek Khadgi <109225495+evoAB@users.noreply.github.com>
2024-04-21 17:34:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function fibonacci(num) {
 // your code here
 	if(num===0 || num===1)
 		return num;
-	return fibonacci(n-1)+fibonacci(n-2);
+	return fibonacci(num-1)+fibonacci(num-2);
 }
 
 module.exports = fibonacci;
